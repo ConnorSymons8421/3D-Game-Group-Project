af50b5a1442bd528f8c9428d576e9d3042a07a9b
ConnorSymons8421 <connorjsymons@gmail.com>
2025-12-16 21:37:01 -0800

    Finished slip and slide and added additional spotlights

--- Assets/Scripts/Launch Zone.cs
+++ Assets/Scripts/Launch Zone.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class LaunchZone : MonoBehaviour
 {
+    public float launchForce = 10f;
+
     void OnCollisionEnter(Collision collision)
     {
         // check for Rigidbody
         Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
         if (rb != null)
         {
-            //rb.AddForce(new Vector3(1, 1, 1) * bounceForce, ForceMode.Impulse); // send player flying forward
+            rb.AddForce(new Vector3(1, 1, 0) * launchForce, ForceMode.Impulse); // send player flying forward
         }
     }
 }
